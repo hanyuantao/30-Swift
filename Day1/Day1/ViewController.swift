6c512a592f51dd693c49569894a42680d3aa222d
oncer <545574484@qq.com>
2017-09-26 00:04:02 +0800

    new begin

---
+++ Day1/Day1/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Day1
+//
+//  Created by An on 2017/9/25.
+//  Copyright © 2017年 once. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var playBtn:UIButton!
+    @IBOutlet weak var pauseBtn:UIButton!
+    @IBOutlet weak var timeLab:UILabel!
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        get{
+            return UIStatusBarStyle.lightContent
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.timeLab.text = "0.00"
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
