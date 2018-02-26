fc6c4a6013eec49b80e7cad11980388127bca7e7
once <545574484@qq.com>
2018-02-26 11:25:43 +0800

    Project7 -End
    学习JSONSwift Tabbar 的用法等

--- Swift-English/Project7/Project7/ViewController.swift
+++ Swift-English/Project7/Project7/ViewController.swift
@@ -10,9 +10,55 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    var petitions = [[String: String]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Project7"
+        
+        let urlString:String
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        } else {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+        }
+        
+        if let url  = URL(string: urlString) {
+            if let data = try? String(contentsOf: url){
+                let json = JSON(parseJSON: data)
+                if json["metadata"]["responseInfo"]["status"] == 200{
+                    parse(json: json)
+                    return
+                }else{
+                    showError()
+                }
+            }else{
+                showError()
+            }
+        }else{
+            showError()
+        }
+
         // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    func parse(json:JSON){
+        for result in json["results"].arrayValue {
+            let title = result["title"].stringValue;
+            let body = result["body"].stringValue
+            let sigs = result["signatureCount"].stringValue
+            let obj = ["title": title, "body": body, "sigs": sigs]
+            petitions.append(obj)
+        }
+        tableView.reloadData()
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +66,23 @@ class ViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CELL", for: indexPath)
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition["title"]
+        cell.detailTextLabel?.text = petition["body"]
+        return cell;
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
 }
 
