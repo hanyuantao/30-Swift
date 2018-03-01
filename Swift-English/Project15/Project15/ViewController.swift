463c38ee874079fef48b696cd05a60c4c37312c2
once <545574484@qq.com>
2018-03-01 11:22:09 +0800

    Project15-Done

--- Swift-English/Project15/Project15/ViewController.swift
+++ Swift-English/Project15/Project15/ViewController.swift
@@ -30,11 +30,26 @@ class ViewController: UIViewController {
     @IBAction func tapped(_ sender: Any) {
         
         tap.isHidden = true
-        UIView.animate(withDuration: 1, delay: 0, options: [], animations: { [unowned self] in
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 5, options: [], animations: { [unowned self] in
             switch self.currentAnimation {
             case 0:
                 self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
-                
+            case 1:
+                self.imageView.transform = CGAffineTransform.identity
+            case 2:
+                self.imageView.transform = CGAffineTransform(translationX: -256, y: -256)
+            case 3:
+                self.imageView.transform = CGAffineTransform.identity
+            case 4:
+                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+            case 5:
+                self.imageView.transform = CGAffineTransform.identity
+            case 6:
+                self.imageView.alpha = 0.1
+                self.imageView.backgroundColor = UIColor.green
+            case 7:
+                self.imageView.alpha = 1
+                self.imageView.backgroundColor = UIColor.clear
             default:
                 break
             }
